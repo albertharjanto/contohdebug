adeb1da4ce03fdb4af36e29eb4c0504d33f2ce2d
albertharjanto <71241075@students.ukdw.ac.id>
2025-02-08 18:06:50 +0700

    contohdebug

---
+++ contohdebug1.py
@@ -0,0 +1,9 @@
+#Contoh keslahan dalam menulis program (syntax error)
+
+bilangan_1 = int(input("Masukkan bilangan pertama: "))
+bilangan_2 = int(input("Masukkan bilangan kedua: "))
+
+if bilangan_1 > bilangan2 :
+    print(f"{bilangan_1} adalah bilangan terbesar")
+else:
+    print(f"{bilangan_2} adalah bilangan terbesar")
---
+++ contohdebug2.py
@@ -0,0 +1,9 @@
+#Contoh keslahan saat program berjalan (runtime error)
+
+bilangan_1 = int(input("Masukkan bilangan pertama: "))
+bilangan_2 = int(input("Masukkan bilangan kedua: "))
+
+if bilangan_1 < bilangan_2 :
+    print(f"{bilangan_1} adalah bilangan terbesar")
+else:
+    print(f"{bilangan_2} adalah bilangan terbesar")
